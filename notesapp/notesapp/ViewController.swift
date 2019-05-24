4864279042869a1ac558912a06bc6e49038a2c28
Vladyslav Kolomiets <vladyslavkolomiets@gmail.com>
2019-05-24 23:37:43 +0300

    Initial commit

---
+++ notesapp/notesapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  notesapp
+//
+//  Created by Vladyslav Kolomiets on 5/24/19.
+//  Copyright © 2019 Vladyslav Kolomiets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
